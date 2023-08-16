e383ce2368c0bbf1d6e4fd345389d107a5eb8e50
swedodson <zerospelling@gmail.com>
2023-08-15 23:31:43 -0700

    Added Edit button to update trips

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const controller = require('../controllers/main');
+
+const tripsController = require('../controllers/trips');
 
 /* GET home page. */
-router.get('/', controller.index);
+router
+    .route('/trips')
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
+
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
